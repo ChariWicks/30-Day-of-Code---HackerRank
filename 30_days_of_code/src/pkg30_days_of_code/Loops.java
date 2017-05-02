e7b61daa32331955986198dc11aa4c325d604f3d
Charitha Wickramanayake <charitha.wickramanayake@gmail.com>
2017-05-02 20:45:51 +0530

    Loops
    Print first ten multiples

--- 30_days_of_code/src/pkg30_days_of_code/Loops.java
+++ 30_days_of_code/src/pkg30_days_of_code/Loops.java
@@ -5,10 +5,27 @@
  */
 package pkg30_days_of_code;
 
+import java.io.Console;
+import java.util.Scanner;
+
 /**
  *
  * @author CHARITHA
  */
+//System;
+//System.Collections.Generic;
+//System.IO;
+//System.Linq;
 public class Loops {
+    
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+        for(int i = 1; i < 11; i++){
+            System.out.println(n + " x " + i + "=" + (n*i));
+        }
+    }
     
 }
