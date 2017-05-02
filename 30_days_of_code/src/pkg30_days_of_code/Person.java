bcba2b41cb79fd7728cf85f4462548546f70da86
Charitha Wickramanayake <charitha.wickramanayake@gmail.com>
2017-05-02 19:00:13 +0530

    Person class
    Classes and Instances

--- 30_days_of_code/src/pkg30_days_of_code/Person.java
+++ 30_days_of_code/src/pkg30_days_of_code/Person.java
@@ -11,48 +11,47 @@ import java.util.Scanner;
  *
  * @author CHARITHA
  */
-public class ClassesInstances {
+public class Person {
     private int age;	
   
-	public ClassesInstances(int initialAge) {
+	public Person(int initialAge) {
   		// Add some more code to run some checks on initialAge
-            
-        if(initialAge > 0){
-            age = initialAge;
-        }else{
+        if(initialAge < 0){
             age = 0;
-            System.out.println("print Age is not valid");
+            System.out.println("Age is not valid, setting age to 0.");
+        }else{
+            age = initialAge;
         }
 	}
 
 	public void amIOld() {
-  		// Write code determining if this person's age is old and print the correct statement:
-            
         if(age < 13){
             System.out.println("You are young.");
-        }
-        if(age >= 13 && age < 18){
+        }else if(age >= 13 && age < 18){
             System.out.println("You are a teenager.");
-        }
-        if(age >= 18){
+        }else{
+            // Write code determining if this person's age is old and print the correct statement:
             System.out.println("You are old.");
         }
-        //System.out.println(/*Insert correct print statement here*/);
-        //System.out.println(age);
-
+  		
 	}
 
 	public void yearPasses() {
   		// Increment this person's age.
-            age++;
+        age++;
 	}
+            
+            
+        
+            
+
         
         public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		for (int i = 0; i < T; i++) {
 			int age = sc.nextInt();
-			ClassesInstances p = new ClassesInstances(age);
+			Person p = new Person(age);
 			p.amIOld();
 			for (int j = 0; j < 3; j++) {
 				p.yearPasses();
@@ -62,7 +61,8 @@ public class ClassesInstances {
         }
 		sc.close();
     }
-}
+    }
+
 
     
 
